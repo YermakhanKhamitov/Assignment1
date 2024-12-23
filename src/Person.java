46da27220313dd3c80dc59daaaee3900aa3e6ecc
YermakhanKhamitov <https://github.com/YermakhanKhamitov/Assignment1.git@>
2024-12-23 09:13:22 +0500

    Create two derived classes Student and Teacher that inherit from Person.

--- src/Person.java
+++ src/Person.java
@@ -4,124 +4,24 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-class Student {
-    private static int lastAssignedID = 0;
-    private int studentID;
-    private String name;
+public class Person {
+    private  String name;
     private String surname;
     private int age;
     private boolean gender;
-    private List<Integer> grades;
 
-    public Student(String name, String surname, int age, boolean gender) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-        this.gender = gender;
-        this.studentID = ++lastAssignedID;
-        this.grades = new ArrayList<>();
+    public Person(String name, String surname, int age, boolean gender) {
+        this.name=name;
+        this.surname=surname;
+        this.age=age;
+        this.gender=gender;
     }
-
-    public void addGrade(int grade) {
-        if (grade >= 0 && grade <= 100) {
-            grades.add(grade);
-        } else {
-            System.out.println("Grade must be between 0 and 100.");
-        }
-    }
-
-    public double calculateGPA() {
-        if (grades.isEmpty()) {
-            return 0.0;
-        }
-        int sum = 0;
-        for (int grade : grades) {
-            sum += grade;
-        }
-        return sum / (double) grades.size();
-    }
-
-    @Override
     public String toString() {
-        String genderStr = gender ? "Male" : "Female";
-        return "Hi, I am " + name + " " + surname + ", a " + age + "-year-old " + genderStr + ". I am a student with ID " + studentID + ".";
+        String genderStr = gender ? "male" : "female";
+        return "Hi, I am" + " " + name + " " + surname + ", " + "a" + " " + age + "-years-old" + ", " + genderStr + "!";
     }
-
-    public static List<Student> readStudentsFromFile(String filename) {
-        List<Student> students = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(" ");
-                String name = parts[0];
-                String surname = parts[1];
-                int age = Integer.parseInt(parts[2]);
-                boolean gender = Boolean.parseBoolean(parts[3]);
-                Student student = new Student(name, surname, age, gender);
-                for (int i = 4; i < parts.length; i++) {
-                    student.addGrade(Integer.parseInt(parts[i]));
-                }
-                students.add(student);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return students;
-    }
-}
-
-class Teacher {
-    private String name;
-    private String surname;
-    private int age;
-    private boolean gender;
-    private String subject;
-    private int yearsOfExperience;
-    private int salary;
-
-    public Teacher(String name, String surname, int age, boolean gender, String subject, int yearsOfExperience, int salary) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-        this.gender = gender;
-        this.subject = subject;
-        this.yearsOfExperience = yearsOfExperience;
-        this.salary = salary;
-    }
-
-    public void giveRaise(double percentage) {
-        if (percentage > 0) {
-            salary += salary * percentage / 100;
-        } else {
-            System.out.println("Percentage must be positive.");
-        }
-    }
-
-    @Override
-    public String toString() {
-        String genderStr = gender ? "Male" : "Female";
-        return "Hi, I am " + name + " " + surname + ", a " + age + "-year-old " + genderStr + ". I teach " + subject + ".";
-    }
-
-    public static List<Teacher> readTeachersFromFile(String filename) {
-        List<Teacher> teachers = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(" ");
-                String name = parts[0];
-                String surname = parts[1];
-                int age = Integer.parseInt(parts[2]);
-                boolean gender = Boolean.parseBoolean(parts[3]);
-                String subject = parts[4];
-                int yearsOfExperience = Integer.parseInt(parts[5]);
-                int salary = Integer.parseInt(parts[6]);
-                Teacher teacher = new Teacher(name, surname, age, gender, subject, yearsOfExperience, salary);
-                teachers.add(teacher);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return teachers;
+    public static void main(String[] args) {
+        Person person = new Person("Yermakhan", "Khamitov", 17, true);
+        System.out.println(person);
     }
 }
